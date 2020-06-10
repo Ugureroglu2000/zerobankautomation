e85aee50667b2fafe4ccdc0836464aa824e579e3
Ugureroglu2000 <ugureroglu2000@gmail.com>
2020-06-10 17:46:25 +0100

    f

--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -366,11 +366,9 @@ formatter.step({
   "name": "user selects type “Deposit”",
   "keyword": "When "
 });
-formatter.match({
-  "location": "com.zerobank.Step_Definitions.FindTransactionsStepDef.user_selects_type_Deposit()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "results table should show at least one result under \"Deposit\"",
@@ -380,7 +378,7 @@ formatter.match({
   "location": "com.zerobank.Step_Definitions.FindTransactionsStepDef.results_table_should_show_at_least_one_result_under(java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "results table should show no result under \"Withdrawal\"",
@@ -390,18 +388,15 @@ formatter.match({
   "location": "com.zerobank.Step_Definitions.FindTransactionsStepDef.results_table_should_show_no_result_under(java.lang.String)"
 });
 formatter.result({
-  "error_message": "org.openqa.selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document\n  (Session info: chrome\u003d83.0.4103.61)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/stale_element_reference.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-HLEND38\u0027, ip: \u0027192.168.43.223\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002713.0.1\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 83.0.4103.61, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: C:\\Users\\HP\\AppData\\Local\\T...}, goog:chromeOptions: {debuggerAddress: localhost:61159}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: b9cd0da7db4deb0d7de48fce3042a9ab\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:285)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.getText(RemoteWebElement.java:166)\r\n\tat com.zerobank.Pages.AccountActivityPage.WithdrawalListString(AccountActivityPage.java:56)\r\n\tat com.zerobank.Step_Definitions.FindTransactionsStepDef.results_table_should_show_no_result_under(FindTransactionsStepDef.java:135)\r\n\tat ✽.results table should show no result under \"Withdrawal\"(file:///C:/Users/HP/IdeaProjects/fromgit/zerobankautomation/src/test/Resources/features/FindTransactions.feature:47)\r\n",
-  "status": "failed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "user selects type “Withdrawal”",
   "keyword": "When "
 });
-formatter.match({
-  "location": "com.zerobank.Step_Definitions.FindTransactionsStepDef.user_selects_type_Withdrawal()"
-});
+formatter.match({});
 formatter.result({
-  "status": "skipped"
+  "status": "undefined"
 });
 formatter.step({
   "name": "results table should show at least one result under \"Withdrawal\"",
@@ -423,7 +418,6 @@ formatter.match({
 formatter.result({
   "status": "skipped"
 });
-formatter.embedding("image/png", "embedded2.png", "screenshot");
 formatter.after({
   "status": "passed"
 });
